b482a5c4349bed04f8d024e13c233ec55cd1d74f
Divya123solanki <solankidivya817@gmail.com>
2024-08-25 15:12:26 -0400

    Add files via upload

---
+++ String6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class String6 {
+    public static void main(String args[]){
+     String s1,s2;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter 1st string");
+     s1=sc.nextLine();
+     System.out.println("Enter 2nd string");
+     s2=sc.nextLine();
+     System.out.println("Concatenated string is");
+     System.out.println(s1+" "+s2);
+
+        }
+    }
+
